cddb99737151ae4f833fbbc6a3a628c400dd8c32
t857057 <39800729+t857057@users.noreply.github.com>
2018-06-12 01:44:43 +0800

    *_*

--- src/dev/jadez/cheatgame/input/MouseManager.java
+++ src/dev/jadez/cheatgame/input/MouseManager.java
@@ -45,7 +45,7 @@ public class MouseManager implements MouseListener,MouseMotionListener {
 				if(my >= 500 && my <= 600) {
 					//System.out.println("HIT");
 					//game.initGame();
-					State.setState(game.getState(1));
+					State.setState(game.getState(11));
 					return;
 				}
 				
